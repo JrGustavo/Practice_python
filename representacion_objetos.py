67e852bc030e414df7dd8a5b51bbe35314fd10e3
JrGustavo <gus@soypetal.com>
2023-05-25 20:35:36 -0500

    Update Course

---
+++ representacion_objetos.py
@@ -0,0 +1,31 @@
+# Representacion de objetos ( str, repr, format)
+# print(dir(object))
+
+class Persona:
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+
+    # repr, mas enfocado a los programadores
+    def __repr__(self):
+        return {self.__class__.__name__ }, apellido: {self.apellido}'
+
+    # Str es mas para el usuario final u otros sistemas
+    # La implementación por default llama al metodo repr
+
+    def __str__(self):
+        return f'{self.__class__.name__}:{self.nombre} {self.apellido}'
+
+    # format su implementacion por default es str
+    def __format__(self, format_spec):
+        return f'{self.__class__.__name__} con nombre {self.nombre}'
+
+
+
+persona1 = Persona('Juan', 'Perez')
+#Repre
+print(f'Mi objeto persona1: {persona1!r}')
+#str (de manera automatica el metodo print llsms al metodo str)
+print(persona1)
+#format
+print(f'{persona1}')
---
+++ sobrecarga_constructores.py
@@ -0,0 +1,31 @@
+# Simulacion de sobrecarga de constructores en Python
+# Otras formas de crear objetos
+
+class Persona:
+
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+
+    @classmethod
+    def crear_persona_vacia(cls):
+        return cls(None, None)
+
+    @classmethod
+    def crear_persona_con_valores(cls, nombre, apellido):
+        return cls(nombre, apellido)
+
+
+    def __str__(self):
+        return f'Nombre:{self.nombre}, Apellido: {self.apellido}'
+
+persona1 = Persona('Juan', 'Perez')
+print(persona1)
+persona_vacia = Persona.crear_persona_vacia()
+print(persona_vacia)
+persona_con_valores = Persona.crear_persona_con_valores('Karla', 'Gomez')
+print(persona_con_valores)
+
+
+
+
